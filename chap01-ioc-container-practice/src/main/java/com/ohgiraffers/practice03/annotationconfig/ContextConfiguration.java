0c0ce2d6bd0c19860e5656971355f324350ea431
gsh118 <hks01108@naver.com>
2024-06-21 19:53:38 +0900

    01 practice

---
+++ chap01-ioc-container-practice/src/main/java/com/ohgiraffers/practice03/annotationconfig/ContextConfiguration.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.practice03.annotationconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ContextConfiguration {
+    public static void main(String[] args) {
+
+    }
+}
